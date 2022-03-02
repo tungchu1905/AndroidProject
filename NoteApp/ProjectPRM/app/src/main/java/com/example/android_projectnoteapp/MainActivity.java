eed7fa7a53bba1d7bd53e2a8b18aae17947a2f7d
tungchu1905 <chutrieutung2000@gmail.com>
2022-03-02 08:44:33 +0700

    still working

---
+++ NoteApp/ProjectPRM/app/src/main/java/com/example/android_projectnoteapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.android_projectnoteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE_ADD_NOTE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView addNewNote = findViewById(R.id.imageAddNoteMain);
+        addNewNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(getApplicationContext(), CreateNoteActivity.class)
+                        ,REQUEST_CODE_ADD_NOTE);
+            }
+        });
+    }
+}
