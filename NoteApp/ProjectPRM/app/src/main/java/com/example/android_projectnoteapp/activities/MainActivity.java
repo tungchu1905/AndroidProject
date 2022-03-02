5f0d850ed1c6240f6b13dec7de8af134ae2d5038
tungchu1905 <chutrieutung2000@gmail.com>
2022-03-02 10:35:49 +0700

    developing

--- NoteApp/ProjectPRM/app/src/main/java/com/example/android_projectnoteapp/activities/MainActivity.java
+++ NoteApp/ProjectPRM/app/src/main/java/com/example/android_projectnoteapp/activities/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.android_projectnoteapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.example.android_projectnoteapp.R;
-import com.example.android_projectnoteapp.activities.CreateNoteActivity;
+import com.example.android_projectnoteapp.database.NoteDatabase;
+import com.example.android_projectnoteapp.entities.Note;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_CODE_ADD_NOTE = 1;
@@ -25,5 +31,29 @@ public class MainActivity extends AppCompatActivity {
                         ,REQUEST_CODE_ADD_NOTE);
             }
         });
+        getNotes();
+    }
+
+    private void getNotes(){
+
+        @SuppressLint("StaticFieldLeak")
+        class  GetNotesTask extends AsyncTask<Void, Void, List<Note>>{
+
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+
+                return NoteDatabase.
+                        getDatabase(getApplicationContext()).noteDao().getAllNotes();
+            }
+
+            @Override
+            protected void onPostExecute(List<Note> notes) {
+                super.onPostExecute(notes);
+                Log.d("My_Notes", notes.toString());
+            }
+        }
+
+        new GetNotesTask().execute();
+
     }
 }
