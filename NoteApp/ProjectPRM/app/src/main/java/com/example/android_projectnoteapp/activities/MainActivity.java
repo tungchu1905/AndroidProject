75a8b60bf0705c00b056ad58621f71e2a54e5ae3
tungchu1905 <chutrieutung2000@gmail.com>
2022-03-04 08:32:50 +0700

    exercise

--- NoteApp/ProjectPRM/app/src/main/java/com/example/android_projectnoteapp/activities/MainActivity.java
+++ NoteApp/ProjectPRM/app/src/main/java/com/example/android_projectnoteapp/activities/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.android_projectnoteapp.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -14,11 +16,15 @@ import com.example.android_projectnoteapp.R;
 import com.example.android_projectnoteapp.database.NoteDatabase;
 import com.example.android_projectnoteapp.entities.Note;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_CODE_ADD_NOTE = 1;
 
+//    private RecyclerView noteRecyclerView;
+//    private List<Note> noteList;
+//    private NoteAdapter noteAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,11 +37,19 @@ public class MainActivity extends AppCompatActivity {
                         ,REQUEST_CODE_ADD_NOTE);
             }
         });
+
+//        noteRecyclerView = findViewById(R.id.noteRecyclerView);
+//        noteRecyclerView.setLayoutManager(
+//                new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
+//        );
+//
+//        noteList = new ArrayList<>();
+//        noteAdapter = new NoteAdapter(noteList);
+//        noteRecyclerView.setAdapter(noteAdapter);
         getNotes();
     }
 
     private void getNotes(){
-
         @SuppressLint("StaticFieldLeak")
         class  GetNotesTask extends AsyncTask<Void, Void, List<Note>>{
 
@@ -49,11 +63,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(List<Note> notes) {
                 super.onPostExecute(notes);
-                Log.d("My_Notes", notes.toString());
+
             }
         }
 
         new GetNotesTask().execute();
 
     }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if(requestCode == REQUEST_CODE_ADD_NOTE && resultCode == RESULT_OK){
+//            getNotes();
+//        }
+//    }
 }
